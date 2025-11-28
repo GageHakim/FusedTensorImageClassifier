a647786ceb972a30fc5d5b75618eec408f25d878
mea432 <easonn999@gmail.com>
2025-11-28 11:29:15 -0500

    pls work

---
+++ hific/build/temp.macosx-10.9-x86_64-cpython-37/CMakeFiles/hific_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/gagecaesarhakim/Desktop/Eaason/hific/hific_cpp.cpython-313-darwin.so"
+  "/Users/gagecaesarhakim/Desktop/Eaason/hific/hific_cpp.pdb"
+  "CMakeFiles/hific_cpp.dir/ans.cpp.o"
+  "CMakeFiles/hific_cpp.dir/ans.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hific_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hific/build/temp.macosx-11.1-arm64-cpython-312/CMakeFiles/hific_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/easonni/Downloads/eason/hific/build/lib.macosx-11.1-arm64-cpython-312/hific_cpp.cpython-312-darwin.so"
+  "/Users/easonni/Downloads/eason/hific/build/lib.macosx-11.1-arm64-cpython-312/hific_cpp.pdb"
+  "CMakeFiles/hific_cpp.dir/ans.cpp.o"
+  "CMakeFiles/hific_cpp.dir/ans.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hific_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
